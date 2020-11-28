0ef50cf5eb0e9652430ca73b3a36395e2698fc0d
Edmund Muzoora Bishanga <me.bishanga03@cantab.net>
2020-11-28 10:49:24 +0000

    log gardening: a few more helpful functions sample log file: for basic testing

--- log_gardening.py
+++ log_gardening.py
@@ -14,12 +14,16 @@
 # + logdate_created
 # + logdate_lastmodified
 
-# import sys
+import sys
+import pytest
+from pprint import pprint
 
-# args = sys.argv[1:]
-# assert len(args) >= 1,'Please provide at least one arg: "filepath"'
+args = sys.argv[1:]
+if not args:
+    args = ['./sample_service_log.txt']
+assert len(args) >= 1,'Please provide at least one arg: "filepath"'
 
-# logfile_path = args[0] if args else "./sample_service_log.txt"
+logfile_path = args[0] if args else "./sample_service_log.txt"
 
 def readlog_chunks(logfile_path, chunksize=1024):
     with open(logfile_path, 'r') as f:
@@ -31,23 +35,43 @@ def readlog_chunks(logfile_path, chunksize=1024):
             yield chunk
 
 def printlog_lines(logfile_path):
+    assert type(logfile_path) == str
     file_generator = (line.strip() for line in open(logfile_path, 'r'))
     for line in file_generator:
         if line and line[0].isalnum():
             print("{}".format(line))
 
 def readlog_tail(logfile_path, numbytes=100):
+    assert type(numbytes) == int, "numbytes: expects: +ve number"
     with open(logfile_path, 'rb') as f:
         tail = f.read(f.seek(-numbytes, 2))
     return tail
 
 def  readlog_head(logfile_path, numbytes=100):
+    assert type(numbytes) == int and numbytes > 0, "numbytes: expects: +ve int"
     with open(logfile_path, 'rb') as f:
         head = f.read(numbytes)
     return head
 
 def readlog_offsetchunk(logfile_path, offset=0, numbytes=100):
+    assert type(offset) == int and type(numbytes) == int
+    assert numbytes > 0, "numbytes: expects: +ve int"
     with open(logfile_path, 'rb') as f:
         f.seek(offset)
         chunk = f.read(f.seek(numbytes, 1))
     return chunk
+
+valid_logfilepath = logfile_path
+invalid_numbytes = [-100, 100000000000, 0, -1]
+def test_readlog_head_invalid_input(valid_logfilepath, invalid_numbytes):
+    # input validation
+    print('started: inputs: {} {}'.format(valid_logfilepath, invalid_numbytes))
+    # main 
+    with pytest.raises(AssertionError) as exec_info:
+        output = readlog_head(valid_logfilepath, invalid_numbytes)
+        print('output:\n{}'.format(output))
+    pprint(exec_info)
+    assert 'numbytes' in exec_info.value
+
+    # output verification
+    print('done')
--- main.py
+++ main.py
@@ -1,28 +1,13 @@
-#!/usr/bin/python
+#!usr/bin/python
+
+# python.exe fullpath: "& C:/Users/mebis/AppData/Local/Microsoft/WindowsApps/python.exe"
 
 import log_gardening
 
-from test_classes.Park import Park
-from test_classes.ParkRunner import ParkRunner
-from test_classes.ParkRun import ParkRun
 from pprint import pprint
 
 def main():
     print('this is the main program... Ta!')
-    # Space = Park("Eynesbury", temperature=15, precipitation=2, surface="mixed", inclination=2)
-    # print('Park: Venue: {}'.format(Space.venue))
-    # print('Park: Temp: {}'.format(Space.temperature))
-
-    # Runner = ParkRunner("Edmund M Bishanga", age=36, height=1.75, weight=67, resilience=2, consistency=2, restHR=45, maxHR=200)
-    # print("{}: VO2max_potential: {}".format(Runner.name, Runner.get_vo2max_potential()))
-    # print("{}: BMI: {}".format(Runner.name, Runner.get_bmi()))
-
-    # Race = ParkRun(park=Space, runner=Runner, distance_km=10, time_min=40)
-    # print("{}: 5km_time_min: {}".format(Runner.name, Race.get_t_parkrun_min()))
-    # print("{}: V02_current: {}".format(Runner.name, Race.get_vo2max_current()))
-
-    # normalised_effort = 100 * round((Race.get_vo2max_current() / Runner.get_vo2max_potential()), 2)
-    # print("{}: Normalised Effort: {}%".format(Runner.name, normalised_effort))
 
     # testing log parsing/gardening module.
     logfile = "./sample_service_log.txt"
@@ -54,6 +39,12 @@ def main():
     pprint(tail)
 
     print('\n')
+
+    invalid_numbytes = [-100, 100000000000, 0, -1, 0.01]
+    for numbytes in invalid_numbytes:
+        print('verify: input validation: numbytes: {}'.format(numbytes))
+        output = log_gardening.readlog_head(logfile, numbytes)
+        print('output: {}'.format(repr(output)))
 
 if __name__ == '__main__':
     main()
