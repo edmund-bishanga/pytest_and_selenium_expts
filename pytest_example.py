26c4dd4fb0f845bc4c40273084fb07b77f33a7f1
Edmund M BISHANGA <me.bishanga03@cantab.net>
2021-10-19 16:59:39 +0100

    pytest_example: + examples: green, orange, red scenarios + docstring examples + format err_msg printouts

--- pytest_example.py
+++ pytest_example.py
@@ -4,43 +4,49 @@ import math
 import pytest
 from pprint import pprint
 
-# valid_pairs = [(7, 49), (2, 4), (1, 1), (0, 0)]
-# @pytest.mark.parametrize("p_sqrt, num", valid_pairs)
-# def test_sqrt(num, p_sqrt):
-#    new_obj = dict()
-#    new_obj.update({'abc': 123})
-#    print('DEBUG: new_obj'); pprint(new_obj)
-#    assert math.sqrt(num) == p_sqrt
+valid_pairs = [(7, 49), (2, 4), (1, 1), (0, 0)]
+@pytest.mark.parametrize("p_sqrt, num", valid_pairs)
+def test_sqrt(num, p_sqrt):
+   """ scenario: green: DEBUG printing, during dev """
+   new_obj = dict()
+   new_obj.update({'abc': 123})
+   print('DEBUG: new_obj')
+   pprint(new_obj, width=120)
+   assert math.sqrt(num) == p_sqrt
 
-# @pytest.mark.parametrize("num, p_square", valid_pairs)
-# def test_square(num, p_square):
-#    print(num**2)
-#    err_msg = "invalid pair: ({}, {})".format(num, p_square)
-#    assert num**2 == p_square, err_msg
+@pytest.mark.parametrize("num, p_square", valid_pairs)
+def test_square(num, p_square):
+   """ scenario: green: positive assertion, input data re-use """
+   print(num**2)
+   err_msg = f'Error: invalid pair: ({num}, {p_square})'
+   assert num**2 == p_square, err_msg
 
-# valid_eq_pairs = [(127127127127127, 127127127127127), (7, 7), (2, 2), (1, 1)]
-# @pytest.mark.parametrize("num1, num2", valid_eq_pairs)
-# def test_equality_ints(num1, num2):
-#    err_msg = "{} not equal to {}".format(num1, num2)
-#    assert num1 == num2, err_msg
+valid_eq_pairs = [(127127127127127, 127127127127127), (7, 7), (2, 2), (1, 1)]
+@pytest.mark.parametrize("num1, num2", valid_eq_pairs)
+def test_equality_ints(num1, num2):
+   err_msg = f'{num1} not equal to {num2}'
+   assert num1 == num2, err_msg
 
-valid_inputs = ['retter', 'abccba', 'radar', 'padap']
-@pytest.mark.parametrize('input_word', valid_inputs)
+valid_pallendrums = ['retter', 'abccba', 'radar', 'padap']
+@pytest.mark.parametrize('input_word', valid_pallendrums)
 def test_pallendrum_valid(input_word):
+   """ scenario: green: test positive user input coverage """
    err_msg = f'Error: {input_word}: not a pallendrum'
    assert input_word == input_word[::-1], err_msg
 
-invalid_inputs = ['aretter', 'aabccba', 'aradar', 'invalid']
-@pytest.mark.parametrize('input_word', invalid_inputs)
+invalid_pallendrums = ['aretter', 'aabccba', 'aradar', 'invalid']
+@pytest.mark.parametrize('input_word', invalid_pallendrums)
 def test_pallendrum_invalid(input_word):
+   """ scenario: orange: tests mild erroneous user input """
    err_msg = f'Error: {input_word}: is a pallendrum'
    assert input_word != input_word[::-1], err_msg
 
 invalid_inputs = [1, 2, 3, 4]
 @pytest.mark.parametrize('input_int', invalid_inputs)
 def test_expecting_exception(input_int):
+   """ scenario: red: tests errorHandling: provide specificException name """
    with pytest.raises(Exception):
       input_int / 0
 
+# Add DDT: DataDrivenTesting module experiment
 import ddt
-
