a9f7f8e80d0696d78d339ddca7b312ca90e8da08
Edmund M BISHANGA <me.bishanga03@cantab.net>
2021-10-19 14:04:24 +0100

    pytest: test out valid, invalid, exp_exception

--- pytest_example.py
+++ pytest_example.py
@@ -24,8 +24,23 @@ from pprint import pprint
 #    err_msg = "{} not equal to {}".format(num1, num2)
 #    assert num1 == num2, err_msg
 
-valid_inputs = ['retter', 'abccba', 'radar', 'invalid']
+valid_inputs = ['retter', 'abccba', 'radar', 'padap']
 @pytest.mark.parametrize('input_word', valid_inputs)
-def test_pallendrum(input_word):
-   err_msg = "{}: not a pallendrum".format(input_word)
+def test_pallendrum_valid(input_word):
+   err_msg = f'Error: {input_word}: not a pallendrum'
    assert input_word == input_word[::-1], err_msg
+
+invalid_inputs = ['aretter', 'aabccba', 'aradar', 'invalid']
+@pytest.mark.parametrize('input_word', invalid_inputs)
+def test_pallendrum_invalid(input_word):
+   err_msg = f'Error: {input_word}: is a pallendrum'
+   assert input_word != input_word[::-1], err_msg
+
+invalid_inputs = [1, 2, 3, 4]
+@pytest.mark.parametrize('input_int', invalid_inputs)
+def test_expecting_exception(input_int):
+   with pytest.raises(Exception):
+      input_int / 0
+
+import ddt
+
