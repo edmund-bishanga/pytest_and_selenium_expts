49a267cb559db2ae181b2b9069190730760b8827
Edmund Muzoora Bishanga <me.bishanga03@cantab.net>
2021-12-10 19:48:32 +0000

    selenium experiment: added pytest function tests

--- selenium_strava_ui_test.py
+++ selenium_strava_ui_test.py
@@ -5,19 +5,27 @@ Web UI Test Automation Experiments: Selenium
 """
 
 # pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
 
-import sys
+import json
 from pprint import pprint
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+
+import pytest
 from msedge.selenium_tools import Edge, EdgeOptions
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 URL = 'https://www.strava.com'
 USERNAME = 'me.bishanga@gmail.com'
 CRD = 'foobar1234'
 STD_WAIT_SECONDS = 20
 MAL_EXIT_CODE = 1
+SUPPORTED_BROWSERS = ['chrome', 'firefox', 'edge']
+ENV_CONFIG_FILE = './configs/env_config.json'
+
+class UnsupportedBrowserException(Exception):
+    pass
 
 def login_as_subscriber(browser, username, pswd):
     """ Log in as specified user, in provided browser. """
@@ -31,18 +39,29 @@ def login_as_subscriber(browser, username, pswd):
     crd_textbox.send_keys(pswd + Keys.RETURN)
 
 def interact_with_cookies_banner(browser, accept=True):
+    """ Accept or reject cookies: per user's choice. """
     print(f'\nDEBUG: Interacting with Cookies Banner: Accept: {accept}')
     # if accept: click on appropriate button.
     if accept:
         cookie_accept_btn = browser.find_element(By.CLASS_NAME, 'btn-accept-cookie-banner')
         cookie_accept_btn.click()
 
+def get_user_credentials(username='default_user'):
+    with open(ENV_CONFIG_FILE, 'r', encoding='UTF-8') as env_config_file:
+        env_config = json.load(env_config_file)
+    user_email = env_config.get(username).get('email')
+    user_pswd = env_config.get(username).get('pswd')
+    print(f'\nDEBUG: user_email: {user_email}, user_pswd: {user_pswd}')
+    return (user_email, user_pswd)
+
 def run_selenium_py_website_basics(browser_name):
-    """ Do basic Web UI Actions, using Selenium WebDriver. """
+    """ Action basic web UI user actions, using Selenium WebDriver. """
     browser = get_supported_browser(browser_name)
     browser.get(URL)
     try:
-        login_as_subscriber(browser, USERNAME, CRD)
+        # login_as_subscriber(browser, USERNAME, CRD)
+        user_email, user_pswd = get_user_credentials()
+        login_as_subscriber(browser, user_email, user_pswd)
         for val in [False, True]:
             interact_with_cookies_banner(browser, accept=val)
     except Exception as exc:  # pylint: disable=broad-except
@@ -60,17 +79,33 @@ def get_supported_browser(name):
         return webdriver.Chrome()
     if name.lower() == 'firefox':
         return webdriver.Firefox()
-    if name.lower() == 'microsoft':
+    if name.lower() == 'edge':
         options = EdgeOptions()
         options.use_chromium = True
         return Edge(options=options)
-    print(f"Unsupported browser: {name}")
-    sys.exit(MAL_EXIT_CODE)
+    help_txt = f"Please try again: supported browsers: {SUPPORTED_BROWSERS}"
+    print(f"Unsupported browser: {name}, {help_txt}")
+    raise UnsupportedBrowserException()
+
+@pytest.mark.parametrize('browser', SUPPORTED_BROWSERS)
+def test_get_supported_browser_positive(browser):
+    print(f'\nDEBUG: Browser under Test: supported: {browser.capitalize()}')
+    try:
+        browser_obj = get_supported_browser(browser)
+    finally:
+        browser_obj.close()
+
+unsupported_browsers = ['safari', 'opera', 'ie']
+@pytest.mark.parametrize('browser', unsupported_browsers)
+def test_get_supported_browser_negative(browser):
+    print(f'\nDEBUG: Browser under Test: unsupported: {browser.capitalize()}')
+    with pytest.raises(UnsupportedBrowserException):
+        get_supported_browser(browser)
+
 
 def main():
     """ StartingPoint: Selenium Experiments """
-    ok_browser_names = ['firefox', 'chrome', 'microsoft']
-    for browser in ok_browser_names:
+    for browser in SUPPORTED_BROWSERS:
         print(f'\nDEBUG: Browser: {browser.capitalize()}')
         run_selenium_py_website_basics(browser)
 
