cc711c10ceb8dd1b4daee4b3e4177eed56816126
Edmund Muzoora Bishanga <me.bishanga03@cantab.net>
2022-02-28 18:43:46 +0000

    testing logs module: minor linting improvements + fixed import error + resolved line width inconsistencies: > 80 + minor docstring resolution

--- log_modules/test_log_gardening.py
+++ log_modules/test_log_gardening.py
@@ -8,36 +8,40 @@ Unit Tests for methods in Class: log_gardening.LogGardening
   python -m pytest -v <test_dir>
 """
 
-from unittest import TestCase
+from unittest import TestCase as tc
 
-import pytest
-
-from log_gardening import DEF_LOGFILE, LogGardening
+from .log_gardening import DEF_LOGFILE, LogGardening
 
 
-class TestLogGardening(TestCase):
+class TestLogGardening(tc):
+    """ UnitTestingClass: for methods in LogGardening Class """
 
     def setUp(self):
         # create relevant|necessary obj instances & pre-requisites
-        self.log_handler = LogGardening()
+        self.log_handler = LogGardening(logfile_path=DEF_LOGFILE)
 
     def test_readlog_head_invalid_input(self):
         invalid_numbytes = [-100, -100000000000, 0, -1]
         for invalid_num in invalid_numbytes:
             print(f'\nTesting: invalid numbytes: {invalid_num}')
-            self.log_handler.set_logfile_path(DEF_LOGFILE)
-            with TestCase.assertRaises(self, expected_exception=AssertionError) as context:
+            with tc.assertRaises(
+                self, expected_exception=AssertionError
+            ) as context:
                 self.log_handler.readlog_head(invalid_num)
-            TestCase.assertTrue(self, 'numbytes' in str(context.exception))
+            tc.assertTrue(self, 'numbytes' in str(context.exception))
 
     def test_readlog_tail_invalid_paths(self):
-        equiv_partitions = ['123', 'abc', '@', './sample_service_log.txt']
+        equiv_partitions = [
+            '123', 'abc', '@', './invalid_dir/sample_service_log.txt'
+        ]
         for filepath in equiv_partitions:
             print(f'\nTesting: {filepath}')
             self.log_handler.set_logfile_path(filepath)
-            with TestCase.assertRaises(self, expected_exception=FileNotFoundError) as context:
+            with tc.assertRaises(
+                self, expected_exception=FileNotFoundError
+            ) as context:
                 self.log_handler.readlog_tail()
-            TestCase.assertTrue(self, 'No such file' in str(context.exception))
+            tc.assertTrue(self, 'No such file' in str(context.exception))
 
     def tearDown(self):
         # clean up after unittest run
