8d667ae2f00e34f6269d53d4bd4c1287c161f273
Edmund M BISHANGA <me.bishanga03@cantab.net>
2023-03-23 12:06:30 +0000

    test_log_gardening: improved human readability + error messages + logging

--- tests/test_log_gardening.py
+++ tests/test_log_gardening.py
@@ -12,12 +12,17 @@ Unit Tests for methods in Class: log_gardening.LogGardening
   * add parent/root repo dir to PYTHONPATH or Sys.PATH
     searchable by python import function
 """
+
 import io
+import os
 from contextlib import redirect_stdout
 from unittest import TestCase as tc
 
-from log_modules.log_gardening import DEF_LOGFILE, LogGardening
+from log_modules.log_gardening import LogGardening
 
+LOGS_DIR = os.path.join(os.curdir, '..', 'logs')
+DEF_LOGFILE = os.path.join(LOGS_DIR, 'sample_service_log.txt')
+GECKO_LOGFILE = os.path.join(LOGS_DIR, 'geckodriver.log')
 INVALID_NUMBYTES = [
     -100,
     -100000000000,
@@ -31,33 +36,34 @@ INVALID_PATHS = [
     './invalid_dir/sample_service_log.txt'
 ]
 
-
 class TestLogGardening(tc):
     """ UnitTestingClass: for methods in LogGardening Class """
 
     def setUp(self):
         # create relevant|necessary obj instances & pre-requisites
-        self.log_handler = LogGardening(logfile_path=DEF_LOGFILE)
+        self.log_handler = LogGardening(logfile_path=GECKO_LOGFILE)
 
     def test_printlog_lines_head_valid(self):
         # for each valid input set, validate num_lines printed
+        print(f'\nDEBUG: logfile_path: {self.log_handler.logfile_path}')
         valid_num_lines  = [1, 10, 30, 90, 120]
         for num_lines in valid_num_lines:
-            print(f'\nDEBUG: num_lines: {num_lines}')
+            print(f'\nDEBUG: valid num_lines: {num_lines}')
             with io.StringIO() as buffer, redirect_stdout(buffer):
                 self.log_handler.printlog_lines_head(num_lines)
                 print(f'\nDEBUG: stdout buffer: {buffer.getvalue()}')
+                err_headr = f'{self.log_handler.logfile_path}: expected content'
                 tc.assertIn(
                     self,
-                    member='head',
+                    member='geckodriver	INFO	Listening',
                     container=buffer.getvalue(),
-                    msg='log: missing content'
+                    msg=f'{err_headr}: missing'
                 )
                 tc.assertIsInstance(
                     self,
                     obj=buffer.getvalue(),
                     cls=str,
-                    msg='log: content: invalid type'
+                    msg=f'{err_headr}: invalid type'
                 )
 
     def test_readlog_head_invalid_input(self):
@@ -71,7 +77,7 @@ class TestLogGardening(tc):
 
     def test_readlog_tail_invalid_paths(self):
         for filepath in INVALID_PATHS:
-            print(f'\nTesting: {filepath}')
+            print(f'\nTesting: invalid_paths: {filepath}')
             self.log_handler.set_logfile_path(filepath)
             with tc.assertRaises(
                 self, expected_exception=FileNotFoundError
